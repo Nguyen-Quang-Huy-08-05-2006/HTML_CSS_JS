0acdf8818d3514a378f2a27e6a5c3d36fca47204
Nguyen-Quang-Huy-08-05-2006 <thaohyduonghuy@gmail.com>
2025-02-26 11:37:13 +0700

    a

---
+++ sess16_frontend/demo/demo.js
@@ -0,0 +1,13 @@
+var variable = 5;
+console.log(variable);
+
+let mystr="hello";
+let number=40;
+let strNum=123;
+// console.log(`hello ${mystr},im ${number}`);
+console.log(strNum - number);
+console.log(strNum + number);
+console.log(Math);
+console.log(Math.round(5.5));
+let name = prompt("Nhap vao ten:");
+console.log(name);
