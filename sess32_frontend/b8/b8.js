4596ed8cd5f683f2528f66c1002f74b7df5499db
Nguyen-Quang-Huy-08-05-2006 <thaohyduonghuy@gmail.com>
2025-03-23 20:22:10 +0700

    a

---
+++ sess32_frontend/b8/b8.js
@@ -0,0 +1,17 @@
+const todolist=[
+    {
+        id: 1,
+        content: 'Day luc 7h',
+    },
+    {
+        id: 2,
+        content: 've sinh ca nhan',
+    }
+    {
+        id: 3,
+        content: 'an sang',
+    }
+]
+todolist.forEach(function(item){
+    document
+})
